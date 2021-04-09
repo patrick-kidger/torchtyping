c4612f4827902a0c0fed4835326b684c505b3119
Adil Zouitine <adilzouitinegm@gmail.com>
2021-04-09 19:54:58 +0200

    Support 3.7+
    Add "typing_extensions==3.7.4.3" the user's python version is less than 3.9.
    
    Add 3.7 and 3.8 for test in CI

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import re
 import setuptools
+import sys
 
 here = os.path.realpath(os.path.dirname(__file__))
 
@@ -45,9 +46,14 @@ classifiers = [
     "Framework :: Pytest",
 ]
 
+user_python_version = sys.version_info
+
 python_requires = ">=3.7.0"
 
 install_requires = ["torch>=1.7.0", "typeguard>=2.11.1"]
+
+if user_python_version < (3, 9):
+    install_requires += ["typing_extensions==3.7.4.3"]
 
 entry_points = dict(pytest11=["torchtyping = torchtyping.pytest_plugin"])
 
